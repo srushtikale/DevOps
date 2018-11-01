fb756966ecaa69b5b26080ac85fa47a3345051a6
srushtikale <38000005+srushtikale@users.noreply.github.com>
2018-11-01 13:21:54 +0530

    commenting library

--- create-service-acc_strict.py
+++ create-service-acc_strict.py
@@ -5,7 +5,7 @@ import time
 import re
 import string
 from datetime import datetime
-import cluster_strict
+#import cluster_strict
 
 #def create_service_account(service_account_name, service_account_secret,strict):  
 #force_start_cmd = "rm -rf ~/.dcos && rm -rf /usr/local/bin/dcos && [ -d /usr/local/bin ] || mkdir -p /usr/local/bin && curl https://downloads.dcos.io/binaries/cli/linux/x86-64/dcos-1.10/dcos -o dcos && mv dcos /usr/local/bin && chmod +x /usr/local/bin/dcos && dcos cluster setup "+cluster_strict.CLUSTER_URL+" --insecure --username=bootstrapuser --password=deleteme"
