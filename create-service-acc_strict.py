f65bab1f64f390b5da1f7574e91dfe401c05ec2d
srushtikale <38000005+srushtikale@users.noreply.github.com>
2018-11-01 17:56:58 +0530

    Update create-service-acc_strict.py

--- create-service-acc_strict.py
+++ create-service-acc_strict.py
@@ -6,7 +6,7 @@ import re
 import string
 from datetime import datetime
 #import cluster_strict
-
+CLUSTER_URL = os.environ['CLUSTER_URL']
 #def create_service_account(service_account_name, service_account_secret,strict):  
 #force_start_cmd = "rm -rf ~/.dcos && rm -rf /usr/local/bin/dcos && [ -d /usr/local/bin ] || mkdir -p /usr/local/bin && curl https://downloads.dcos.io/binaries/cli/linux/x86-64/dcos-1.10/dcos -o dcos && mv dcos /usr/local/bin && chmod +x /usr/local/bin/dcos && dcos cluster setup "+cluster_strict.CLUSTER_URL+" --insecure --username=bootstrapuser --password=deleteme"
 
@@ -17,7 +17,7 @@ time.sleep(3)
 
 pkg_task_value = subprocess.check_output('dcos package install dcos-enterprise-cli --yes', shell=True)
   #  sdk_security.delete_service_account('prometheus_service_account','dcos_prometheus_secret')
-#pkg_task_value = subprocess.check_output('curl -k -v '+cluster_strict.CLUSTER_URL+'/ca/dcos-ca.crt -o dcos-ca.crt', shell=True)
+pkg_task_value = subprocess.check_output('curl -k -v '+CLUSTER_URL+'/ca/dcos-ca.crt -o dcos-ca.crt', shell=True)
 pkg_task_value = os.system('export LC_ALL=C.UTF-8')
 pkg_task_value = os.system('export LANG=C.UTF-8')
 pkg_task_value = os.system('dcos security org service-accounts keypair prometheus-private-key.pem prometheus-public-key.pem')
